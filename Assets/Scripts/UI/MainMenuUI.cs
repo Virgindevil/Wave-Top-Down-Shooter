06230c4b62e935e3d963caacd1eb7f439e05b0e7
matuzov <virginven0m@gmail.com>
2026-02-25 08:34:48 +0400

    change load method to load by index

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,13 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+    private int _newGameSceneIndex = 1;
+
     public void StartNewGame()
     {
-        SceneManager.LoadScene("Game",LoadSceneMode.Single);
+        SceneManager.LoadScene(_newGameSceneIndex);
     }
 
     public void ExitGame()
